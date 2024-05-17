40ef8a6c9bad6e872f0269a18840ab131a7cc3e3
maxistl <mstella@udesa.edu.ar>
2024-05-17 11:35:54 -0300

    Actividades

---
+++ clase-17-05-js/experimento.js
@@ -0,0 +1,31 @@
+let mensajeNombre = "Ingrese su nombre"
+let resultadoNombre = prompt(mensajeNombre)
+let nombre = resultadoNombre 
+console.log(nombre)
+
+
+let mensajeEdad = "Ingrese su edad"
+let resultadoEdad = prompt(mensajeEdad)
+let edad = resultadoEdad
+console.log(edad)
+
+
+let mensajeDeporte = "¿Te gustan los deportes?"
+let resultadoDeporte = confirm(mensajeDeporte)
+let fanDeportes =  resultadoDeporte
+console.log(fanDeportes)
+
+
+let mensajeGracias = `Muchas gracias ${nombre} por responder nuestras preguntas`;
+let resultadoGracias = alert(mensajeGracias)
+console.log (resultadoGracias)
+
+
+
+let deportistaProfesional = {
+    nombreUsuario: `${nombre}`,
+    edadUsuario: `${edad}`,
+    fanDeportes :`${resultadoDeporte},
+    profesional: function () 
+
+}
---
+++ clase-17-05-js/precalentando.js
@@ -0,0 +1,14 @@
+let mensaje = "¿Vas a salir de tu casa?"
+let resultado = confirm (mensaje)
+
+if (resultado == true) { //si pongo (resultado) solo, lo toma como true directamente
+    alert("Chequeá el pronóstico del tiempo antes de salir.")
+} else {
+    alert("Que bueno que te quedes. Va a llover mucho hoy.")
+}
+
+
+
+
+
+
